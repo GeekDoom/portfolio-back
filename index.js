39b8ae093c8234faa15ae681698084aa7f8afbfd
Jose Goyos <josegoyos1@gmail.com>
2022-07-12 12:10:12 -0500

    backent exprees portfolio v3.2

--- index.js
+++ index.js
@@ -15,10 +15,15 @@ dbConnection();
 app.use(express.static('public'));
 
 //Cors
-app.use(cors({
-    origin: 'https://josegoyos.vercel.app/',
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}))
+app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization', 'http://localhost:4200', 'https://josegoyos.vercel.app');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 
 //Lectura y parseo del body
